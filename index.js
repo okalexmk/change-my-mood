26b9e31d7f2c80c8cd8f50541607004b2580c3ac
okalexmk <46636085+okalexmk@users.noreply.github.com>
2019-03-27 15:09:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,93 @@
 $(document).ready(function() {
 
 
-  let n=0;
-  console.log('yoo');
+  
 
-  $("#start").on('click',function(){
-    console.log("hello")
-     $("intro").hide();
-    n++
-    console.log(n)
-     $(`question:nth-child(${n}`).show(); 
-    });
+var n = 0;
 
 
-
-  $(".next").on('click',function(){
-    console.log('test')
-    $(`question:nth-child(${n}`).hide();
-    n++
-    console.log(n)
-    $(`question:nth-child(${n})`).show(); 
-     });
-
-
-  $(".previous").on('click',function(){
-     if (n==1) {
-     $(`question:nth-child(${n}`).hide(); 
-    $("intro").show();
-     }
-     $(`question:nth-child(${n}`).hide();
-    n--
-    $(`question:nth-child(${n})`).show() 
-    }); 
-
-  $("#finish").on('click',function(){
-    $(`question:nth-child(${n})`).hide();
-    $("#result").show()
-    for (let i=0; i<=n; i++){
-      let answer = $(`question:nth-child(${i}) textarea`).val();
-      $(`#resultlist td:nth-child(${i})`).text(answer);
-    }
-        
-       
-    }); 
-
+$("#start").on("click", function() {
+  n=0;
+  $("intro").hide();
+  n++;
+  console.log(n);
+  $(`question:nth-child(${n}`).show();
 });
 
+$(".next").on("click", function() {
+  console.log("test");
+  $(`question:nth-child(${n}`).hide();
+  n++;
+  console.log(n);
+  $(`question:nth-child(${n})`).show();
+});
+
+$(".previous").on("click", function() {
+  if (n == 1) {
+    $(`question:nth-child(${n}`).hide();
+    $("intro").show();
+  }
+  $(`question:nth-child(${n}`).hide();
+  n--;
+  $(`question:nth-child(${n})`).show();
+});
+
+$("#finish").on("click", function() {
+  $(`question:nth-child(${n})`).hide();
+  $("#result").show();
+  for (let i = 0; i <= n; i++) {
+    let answer = $(`question:nth-child(${i}) textarea`).val();
+    console.log(answer);
+    if (i == 1) {
+      $("#one").text(answer);
+    }
+    if (i == 2) {
+      $("#two").text(answer);
+    }
+    if (i == 3) {
+      $("#three").text(answer);
+    }
+    if (i == 4) {
+      $("#four").text(answer);
+    }
+    if (i == 5) {
+      $("#five").text(answer);
+    }
+    if (i == 6) {
+      $("#six").text(answer);
+    }
+    if (i == 7) {
+      $("#seven").text(answer);
+    }
+  }
+});
+
+$("#myBtn").on("click", function() {
+   if($('#more').css('display') === 'block'){
+     console.log("test");
+     $("#more").hide();
+    document.getElementById("myBtn").innerHTML = "read more about CBT";
+    }
+   else {
+      $("#more").show();
+    document.getElementById("myBtn").innerHTML = "read less about CBT";
+    }
+});
+
+$(".home").on("click", function() {
+  $("#result").hide();
+  $("#acttwo").hide();
+  console.log(n);
+  $("intro").show();
+});
+
+$("#nextactivity").on("click", function() {
+  $("#result").hide();
+  console.log(n);
+  $("#acttwo").show();
+});
+
+}
 
 
 
